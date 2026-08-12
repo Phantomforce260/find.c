075fe070db766727ebff4e204999269bf2386299
Adrian <aakeem0920@gmail.com>
2026-08-12 16:25:59 -0400

    Adding parser

--- main.c
+++ main.c
@@ -1,160 +1,211 @@
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <dirent.h>
 #include <sys/stat.h>
 
-typedef struct {
-    char** items;
-    size_t count;
-    size_t capacity;
-} StringArray;
+#include "find.h"
 
-static void add_item(StringArray* results, const char* path) {
-
-    if (results->count == results->capacity) {
-        size_t new_capacity = results->capacity == 0
-            ? 16
-            : results->capacity * 2;
-
-        char** new_items = realloc(
-            results->items,
-            new_capacity * sizeof(char*)
-        );
-
-        if (!new_items) {
-            puts("Error in reallocation.");
-            exit(EXIT_FAILURE);
-        }
-
-        results->items = new_items;
-        results->capacity = new_capacity;
+int help(int code, const char* args) {
+    switch (code) {
+        case 0:
+            puts(
+                "find.c - Find and operate on files with readable syntax.\n\n"
+                "SYNOPSIS\n"
+                "    find [type]\n"
+                "    in [directory]\n"
+                "    where [condition]\n"
+                "    then [action]\n"
+            );
+            return EXIT_SUCCESS;
+        case 1:
+            printf("find expects [type] of \"files\" or \"folders\". Received \"%s\" ", args);
+            return EXIT_FAILURE;
+        case 2:
+            puts("find expects a path after \"in\".");
+            return EXIT_FAILURE;
+        case 3:
+            puts("Incomplete Condition.");
+            return EXIT_FAILURE;
+        default:
+            return EXIT_FAILURE;
     }
-
-    results->items[results->count] = strdup(path);
-
-    if (!results->items[results->count]) {
-        puts("Error in strdup");
-        exit(EXIT_FAILURE);
-    }
-
-    results->count++;
 }
 
-static void search_recursive(
-    const char* path,
-    int want_files,
-    StringArray* results
-) {
-    DIR* dir = opendir(path);
-    if (!dir)
-        return;
-
-    struct dirent* entry;
-
-    while ((entry = readdir(dir)) != NULL) {
-        if (strcmp(entry->d_name, ".") == 0 ||
-            strcmp(entry->d_name, "..") == 0)
-            continue;
-
-        char fullpath[4096];
-
-        snprintf(
-            fullpath,
-            sizeof(fullpath),
-            "%s/%s",
-            path,
-            entry->d_name
-        );
-
-        struct stat st;
-
-        if (lstat(fullpath, &st) != 0)
-            continue;
-
-        if (want_files && S_ISREG(st.st_mode)) {
-            add_item(results, fullpath);
-        } else if (!want_files && S_ISDIR(st.st_mode)) {
-            add_item(results, fullpath);
-        }
-
-        if (S_ISDIR(st.st_mode)) {
-            search_recursive(fullpath, want_files, results);
-        }
-    }
-
-    closedir(dir);
-}
-
-static StringArray search(const char* type) {
-    StringArray results = {
-        .items = NULL,
-        .count = 0,
-        .capacity = 0
-    };
-
-    int want_files;
-
-    if (strcmp(type, "files") == 0)
-        want_files = 1;
-    else if (strcmp(type, "folders") == 0)
-        want_files = 0;
-    else
-        return results;
-
-    search_recursive(".", want_files, &results);
-
-    return results;
-}
-
-static void free_results(StringArray* results) {
-    for (size_t i =0; i < results->count; i++) {
-        free(results->items[i]);
-    }
-
-    free(results->items);
-
-    results->items = NULL;
-    results->count = 0;
-    results->capacity = 0;
-}
-
-void help() {
-    puts(
-        "find.c - Find and operate on files with readable syntax.\n\n"
-        "SYNOPSIS\n"
-        "    find [type]\n"
-        "    in [directory]\n"
-        "    where [condition]\n"
-        "    then [action]\n"
-    );
+bool str_equals(const char* str1, const char* str2) {
+    return strcmp(str1, str2) == 0;
 }
 
 int main(int argc, char* argv[]) {
 
     /* If only binary name passed, display help. */
-    if (argc == 1) {
-        help();
-        return EXIT_SUCCESS;
+    if (argc == 1)
+        return help(0, NULL);
+
+    int i = 1;
+    FindCommand command = {0};
+    ParserState state = STATE_TYPE;
+
+    while (state != STATE_DONE && state != STATE_ERROR) {
+        switch (state) {
+            case STATE_TYPE:
+                if (str_equals(argv[i], "files"))
+                    command.type = TYPE_FILES;
+                else if (str_equals(argv[i], "folders"))
+                    command.type = TYPE_FOLDERS;
+                else
+                    return help(1, argv[i]);
+
+                i++;
+
+                if (i >= argc) {
+                    AddString(&command.paths, ".");
+                    command.action = ACTION_PRINT;
+                    state = STATE_DONE;
+                }
+                else {
+                    i++;
+                    state = STATE_PATHS;
+                }
+
+                break;
+            case STATE_PATHS:
+                while (i < argc && !str_equals(argv[i], "where") && !str_equals(argv[i], "then")) {
+
+                    if (str_equals(argv[i], "and")) {
+                        i++;
+                        continue;
+                    }
+
+                    AddString(&command.paths, argv[i]);
+                    i++;
+                }
+
+                if (command.paths.count == 0)
+                    return help(2, NULL);
+                else if (i >= argc) {
+                    command.action = ACTION_PRINT;
+                    state = STATE_DONE;
+                }
+                else if (str_equals(argv[i], "where")) {
+                    i++;
+                    state = STATE_CONDITION;
+                }
+                else if (str_equals(argv[i], "then")) {
+                    i++;
+                    state = STATE_ACTION;
+                }
+
+                break;
+            case STATE_CONDITION:
+
+                if (i >= argc || str_equals(argv[i], "name"))
+                    return help(3, NULL);
+
+                i++;
+
+                if (i >= argc) {
+                    fprintf(stderr, "Expected condition operator.\n");
+                    state = STATE_ERROR;
+                    break;
+                }
+
+                if (strcmp(argv[i], "contains") == 0)
+                    command.condition = CONDITION_CONTAINS;
+                else if (strcmp(argv[i], "endswith") == 0)
+                    command.condition = CONDITION_ENDSWITH;
+                else {
+                    fprintf(
+                        stderr,
+                        "Unknown condition: %s\n",
+                        argv[i]
+                    );
+
+                    state = STATE_ERROR;
+                    break;
+                }
+
+                i++;
+
+                if (i >= argc) {
+                    fprintf(stderr, "Expected condition value.\n");
+                    state = STATE_ERROR;
+                    break;
+                }
+
+                command.value = argv[i];
+                i++;
+
+                if (i >= argc)
+                    state = STATE_DONE;
+                else if (strcmp(argv[i], "then") == 0) {
+                    i++;
+                    state = STATE_ACTION;
+                }
+                else {
+                    fprintf(
+                        stderr,
+                        "Expected \"then\" or end of command.\n"
+                    );
+
+                    state = STATE_ERROR;
+                }
+
+                break;
+            case STATE_ACTION:
+                if (i >= argc || strcmp(argv[i], "move") != 0) {
+                    fprintf(stderr, "Expected action.\n");
+                    state = STATE_ERROR;
+                    break;
+                }
+
+                i++;
+
+                if (i >= argc || strcmp(argv[i], "to") != 0) {
+                    fprintf(stderr, "Expected \"to\" after \"move\".\n");
+                    state = STATE_ERROR;
+                    break;
+                }
+
+                i++;
+
+                if (i >= argc) {
+                    fprintf(stderr, "Expected destination path.\n");
+                    state = STATE_ERROR;
+                    break;
+                }
+
+                //command.has_action = true;
+                //command.action = argv[i];
+
+                i++;
+
+                if (i != argc) {
+                    fprintf(stderr,
+                        "Unexpected argument after action: %s\n",
+                        argv[i]
+                    );
+
+                    state = STATE_ERROR;
+                }
+                else
+                    state = STATE_DONE;
+
+                break;
+
+            case STATE_DONE:
+            case STATE_ERROR:
+                break;
+        }
     }
 
-    /* If only [type] passed, recursive search for all [type]s in cwd */
-    if (argc == 2) {
+    StringArray results = search(&command);
+    if (command.action == ACTION_PRINT)
+        for (size_t j = 0; j < results.count; j++)
+            printf("%s\n", results.items[j]);
 
-        if (strcmp(argv[1], "files") != 0 && strcmp(argv[1], "folders") != 0) {
-            puts("Type must be \"files\" or \"folders\".");
-            return EXIT_FAILURE;
-        }
+    FreeList(&results);
 
-        StringArray results = search(argv[1]);
-
-        for (size_t i = 0; i < results.count; i++) {
-            printf("%s\n", results.items[i]);
-        }
-
-        free_results(&results);
-
-        return EXIT_SUCCESS;
-    }
+    return EXIT_SUCCESS;
 }
